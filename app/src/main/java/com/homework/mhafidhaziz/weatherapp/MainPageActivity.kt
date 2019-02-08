3e8ed4492630241c34ce628dfa28519f861f134e
AD\hafidh.aziz <hafidh.aziz@metraplasa.co.id>
2019-02-08 16:37:29 +0700

    initial commit

---
+++ app/src/main/java/com/homework/mhafidhaziz/weatherapp/MainPageActivity.kt
@@ -0,0 +1,12 @@
+package com.homework.mhafidhaziz.weatherapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_page)
+    }
+}
