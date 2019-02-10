2f624c2d9be3ec1cce6b16e733e4329c6f6145f0
M Hafidh Abdul Aziz <aziz.hfdh@gmail.com>
2019-02-10 20:02:35 +0700

    add view checking test

--- app/src/androidTest/java/com/homework/mhafidhaziz/weatherapp/WeatherActivityTest.kt
+++ app/src/androidTest/java/com/homework/mhafidhaziz/weatherapp/WeatherActivityTest.kt
@@ -5,6 +5,7 @@ import android.support.test.espresso.assertion.ViewAssertions
 import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
+import com.homework.mhafidhaziz.weatherapp.R.id.bottom_sheet
 import com.homework.mhafidhaziz.weatherapp.R.id.forecastRecycler
 import com.homework.mhafidhaziz.weatherapp.presentation.weather.WeatherActivity
 import org.junit.Rule
@@ -26,14 +27,15 @@ class WeatherActivityTest {
     var weatherActivityTestRule = ActivityTestRule(WeatherActivity::class.java)
 
     @Test
-    fun mainActivityTest() {
+    fun launch_weather_app_test() {
+        delay()
+
+        Espresso.onView(ViewMatchers.withId(bottom_sheet))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         delay()
 
         Espresso.onView(ViewMatchers.withId(forecastRecycler))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        delay()
-
-        Espresso.pressBack()
         delay()
 
     }
